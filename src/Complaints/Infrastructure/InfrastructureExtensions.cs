78f2d15a696b99a654a0f8e038bd567e60e808ea
odinous95 <mjd.thif@icloud.com>
2026-09-06 15:19:32 +0100

    refactro: add DI extension for infrastructure layer in complaint

---
+++ src/Complaints/Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Complaints.Application.Contracts;
+using Complaints.Infrastructure.Presistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Complaints.Infrastructure
+{
+    public static class InfrastructureExtensions
+    {
+        extension(IServiceCollection services)
+        {
+            public IServiceCollection AddComplaintsInfrastructure(string connectionString)
+            {
+                // Database
+                services.AddDbContextFactory<ComplaintDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+                // Infrastructure
+                services.AddScoped<IComplaintRepo, ComplaintRepo>();
+
+                return services;
+            }
+        }
+    }
+}
