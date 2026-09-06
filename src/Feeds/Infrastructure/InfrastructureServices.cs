2921500733b73530051e2926ae63a74c7e260905
odinous95 <mjd.thif@icloud.com>
2026-09-06 15:18:10 +0100

    refactor: DI extension file

---
+++ src/Feeds/Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,28 @@
+﻿using Feeds.Application.Contracts;
+using Feeds.Infrastructure.Presistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Feeds.Infrastructure
+{
+    public static class InfrastructureServices
+    {
+
+        extension(IServiceCollection services)
+        {
+            public IServiceCollection AddFeedInfrastructure(string connectionString)
+            {
+                // Database
+                services.AddDbContextFactory<FeedDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+                // Infrastructure
+                services.AddScoped<IFeedRepo, FeedRepo>();
+
+                return services;
+            }
+        }
+    }
+}
---
+++ src/Houses/Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Houses.Application.Contracts;
+using Houses.Infrastructure.Presistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Houses.Infrastructure
+{
+    public static class InfrastructureExtensions
+    {
+        extension(IServiceCollection services)
+        {
+            public IServiceCollection AddHousesInfrastructure(string connectionString)
+            {
+                // Database
+                services.AddDbContextFactory<HouseDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+                // Infrastructure
+                services.AddScoped<IHouseRepo, HouseRepo>();
+
+                return services;
+            }
+        }
+    }
+}
+
