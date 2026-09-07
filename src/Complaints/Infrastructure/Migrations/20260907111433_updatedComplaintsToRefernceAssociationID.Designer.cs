d566ade7486450ea630d6d05f8be71f73564d15a
odinous95 <mjd.thif@icloud.com>
2026-09-07 12:26:46 +0100

    complaint db migration: created new migration with a relational refernce to Association

--- src/Complaints/Infrastructure/Migrations/20260907111433_updatedComplaintsToRefernceAssociationID.Designer.cs
+++ src/Complaints/Infrastructure/Migrations/20260907111433_updatedComplaintsToRefernceAssociationID.Designer.cs
@@ -4,6 +4,7 @@ using Complaints.Infrastructure.Presistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Complaints.Infrastructure.Migrations
 {
     [DbContext(typeof(ComplaintDbContext))]
-    partial class ComplaintDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260907111433_updatedComplaintsToRefernceAssociationID")]
+    partial class updatedComplaintsToRefernceAssociationID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Complaints/Infrastructure/Migrations/20260907111433_updatedComplaintsToRefernceAssociationID.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Complaints.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedComplaintsToRefernceAssociationID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Complaints");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AssociationId",
+                table: "Complaints",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssociationId",
+                table: "Complaints");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Complaints",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
